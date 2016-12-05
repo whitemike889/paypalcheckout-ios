7faf5c0282456c9881160570d747b2bf93be733a
Derek Anderson <dereanderson@paypal.com>
2016-12-05 13:19:48 -0800

    added bridge header for swift to distribution git

---
+++ PYPLCheckoutBridge.h
@@ -0,0 +1,12 @@
+//
+//  PYPLCheckoutBridge.h
+//
+//  Copyright © 2016 PayPal. All rights reserved.
+//
+
+#ifndef PYPLCheckoutBridge_h
+#define PYPLCheckoutBridge_h
+
+#import "PYPLCheckout.h"
+
+#endif /* PYPLCheckoutBridge_h */
